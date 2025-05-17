330c656f47075b6d8981083ff6af2e8a9165f7e0
Alexey <vlad.shabalin.1992@mail.ru>
2025-05-17 04:49:07 +0300

    [XCELL]
    Fix fomod

--- version/scripts.ps1
+++ version/scripts.ps1
@@ -1,6 +1,10 @@
-[int]$buildverint = Get-Content -Path ".\Version\build_version.txt"
-$buildverint+1 | out-file -FilePath ".\Version\build_version.txt" -Force
+[int]$buildverint = Get-Content -Path ".\version\build_version.txt"
+$buildverint+1 | out-file -FilePath ".\version\build_version.txt" -Force
 
-$verfile = Get-Content -Path ".\Version\resource_version2.tmp"
+$verfile = Get-Content -Path ".\version\resource_version2.tmp"
 $verfile = $verfile -Replace "<BUILD>", $buildverint
-$verfile | out-file -FilePath ".\Version\resource_version2.h" -Force
+$verfile | out-file -FilePath ".\version\resource_version2.h" -Force
+
+$verfile = Get-Content -Path ".\version\fomod_info.tmp"
+$verfile = $verfile -Replace "<BUILD>", $buildverint
+$verfile | out-file -FilePath ".\fomod\info.xml" -Force
